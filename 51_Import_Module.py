c9b99af92727c5de4b451162de59f85981a5af47
Akhileswar <akhilkamale@gmail.com>
2025-10-17 05:49:24 +0530

    added files

---
+++ 51_Import_Module.py
@@ -0,0 +1,50 @@
+# Import module in Python
+# In Python, modules help organize code into reusable files. They allow you to import and use functions, classes and variables from other scripts.
+
+# Importing built-in Module
+import math
+pie = math.pi
+print("Value of pi:", pie)
+
+
+
+# Importing External Modules
+# To use external modules, we need to install them first, we can easily install any external module using pip command in the terminal, for example:
+
+# pip install "module_name"
+
+import pandas
+
+# Create a simple DataFrame
+data = {
+    "Name": ["Elon", "Trevor", "Swastik"],
+    "Age": [25, 30, 35]
+}
+
+df = pandas.DataFrame(data)
+print(df)
+
+
+# Importing Specific Functions
+from math import pi
+print(pi)
+
+
+# Importing Modules with Aliases
+import math as m
+result = m.sqrt(25)
+print("Square root of 25:", result)
+
+
+# Importing Everything from a Module (*)
+from math import *
+print(pi)         # Accessing the constant 'pi'
+print(factorial(6))  # Using the factorial function
+
+
+# Handling Import Errors in Python
+try:
+    import mathematics  # Incorrect module name
+    print(mathematics.pi)
+except ImportError:
+    print("Module not found! Please check the module name or install it if necessary.")
---
+++ 52_Virtul_env.py
@@ -0,0 +1,36 @@
+# A virtual environment is an isolated Python environment that allows you to manage dependencies for each project separately. It prevents conflicts between projects and avoids affecting the system-wide Python installation.
+
+# How to Create a Virtual Environment in Python
+# We use the virtualenv module to create isolated environments. It creates a folder with all necessary executables for the project.
+
+# Step 1: Installing virtualenv
+
+$ pip install virtualenv
+
+# Step 2: Check Installation
+
+$ virtualenv --version
+
+# Step 3: Create a Virtual Environment
+
+$ virtualenv my_env
+
+
+# Activating a Virtual Environment in Python
+
+# 1. On Windows
+cd <envname> Scripts\activate
+
+# 2. On Linux/macOS
+$ source virtualenv_name/bin/activate
+
+
+# Installing Dependencies in Virtual Environment
+
+# For example, to install Django 1.9:
+(virtualenv_name)$ pip install Django==1.9
+
+
+# Deactivate Python Virtual Environment
+(virtualenv_name)$ deactivate
+
