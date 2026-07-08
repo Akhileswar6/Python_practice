aa7eecf263e8cca7ccde65ce65eb4396da0b594b
Akhileswar Kamale <akhilkamale@gmail.com>
2026-07-08 16:14:53 +0530

    feat: add bubblr sort

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+        
+    return arr
+    
+print(bubble_sort([89,76,34,23,1,5,6,9]))
